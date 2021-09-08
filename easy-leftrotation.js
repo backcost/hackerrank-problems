d35b0795fc2e6a593695e0017b82f4657b74c19e
Gabriel Costa <gabriel_costa_ferreira@hotmail.com>
2021-09-08 12:05:22 -0300

    done + Big O Notation

--- easy-leftrotation.js
+++ easy-leftrotation.js
@@ -3,19 +3,22 @@ array's elements 1 unit to the left. Given an integer, d, rotate the
 array that many steps left and return the result. */
 
 const rotateLeft = (d, arr) => {
-    let rotated = []
+    let rotated = [] //1
 
-    for (let i = d; i < arr.length; i++) {
+    for (let i = d; i < arr.length; i++) { //2 + 3n
         rotated.push(arr[i])
     }
-    for (let i = 0; i < d; i++) {
+    for (let i = 0; i < d; i++) { //2 + 3n
         rotated.push(arr[i])
     }
     return rotated
-}
+} 
 
 console.log(rotateLeft(4, [1, 2, 3, 4, 5]))
 //[5, 1, 2, 3, 4]
 
 console.log(rotateLeft(2, [1, 2, 3, 4, 5]))
 //[3, 4, 5, 1, 2]
+
+// 5 + 6n
+// O(n)
