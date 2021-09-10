b47d9d6d8aed7c74f90c5b02a6943d710b6bcb2f
Gabriel Costa <gabriel_costa_ferreira@hotmail.com>
2021-09-10 11:47:09 -0300

    done || countingFactors Function + Big O Notation

--- easy-betweenTwoSets.js
+++ easy-betweenTwoSets.js
@@ -11,35 +11,43 @@ Determine how many such numbers exist. */
 
 const getTotalX = (a, b) => {
     const compare = (a, b) => a - b
-    a.sort(compare)
-    b.sort(compare)
 
-    let between = 0
-    const aLast = a[a.length-1]
-    const bLast = b[b.length-1]
-
-    for (let actual = aLast; actual <= bLast; actual += aLast) {
-        let isFactor = true
-        for (let el of a) {
-            if (actual % el !== 0) {
-                isFactor = false
-                break
-            }
-        }
-        if (isFactor) {
-            for (let el of b) {
-                if (el % actual !== 0) {
+    const countingFactors = (first, last) => {
+        let sum = 0
+        for (let actual = first; actual <= last; actual += first) {
+            let isFactor = true
+            for (let el of a) {
+                if (actual % el !== 0) {
                     isFactor = false
                     break
                 }
             }
             if (isFactor) {
-                between++
+                for (let el of b) {
+                    if (el % actual !== 0) {
+                        isFactor = false
+                        break
+                    }
+                }
+                if (isFactor) {
+                    sum++
+                }
             }
         }
+        return sum
     }
-    return between
+
+    a.sort(compare); b.sort(compare)
+    const aLast = a[a.length-1] 
+    const bLast = b[b.length-1]
+
+    return countingFactors(aLast, bLast/2) + countingFactors(bLast, bLast)
 }
+
+// n/2 * (1 + 2n + 1 + 2n + 2)
+// n/2 * (4n + 4)
+// 2n² + 2n
+// O(n²)
 
 console.log(getTotalX([2, 6],[24, 36])) //2
 console.log(getTotalX([2, 4],[16, 32, 96])) //3
